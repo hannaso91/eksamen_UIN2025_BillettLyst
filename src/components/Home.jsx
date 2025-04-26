2019c9e7a2468c4a380583df3399eb301ff77588
hannaso91 <hannaso@hiof.no>
2025-04-26 20:53:04 +0200

    får ut flere eventer i konsollen, men ikke etter navn på eventet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,15 +9,10 @@ export default function Home() {
     const filterByKeyword = ["Findings", "Neon", "Skeikampfestivalen", "Tons of Rock"]
 
     const getEventsByKeywords = async() => {
-        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=AFEfcxa4XlCTGJA56Jk356h0NkfziiWD`)
+        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=100&apikey=AFEfcxa4XlCTGJA56Jk356h0NkfziiWD`)
         .then(response => response.json())
         .then(data => {
-            const allEvents = data._embedded?.events || [];
-            const filtered = allEvents.filter(event => 
-                filterByKeyword.some(keyword => event.name.toLowerCase().includes(keyword.toLowerCase()))
-              );
-            console.log(filtered)
-        setEventsByKeywords(filtered);
+            console.log(data)
       })
       .catch(error => console.error("Feil ved henting av arrangementer:", error));
     }
