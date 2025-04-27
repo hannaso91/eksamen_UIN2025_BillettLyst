e0821b1bf4d5d9957a7744e2e1ff42772975f17d
hannaso91 <hannaso@hiof.no>
2025-04-27 21:29:22 +0200

    pusher siste endringer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,38 +4,81 @@ import { useEffect } from "react";
 
 export default function Home() {
 
-    const [eventsByKeywords, setEventsByKeywords] = useState([])
+    const [eventsByNeon, setEventsByNeon] = useState([])
+    const [eventsByFindings, setEventsByFindings] = useState([])
 
-    const filterByKeyword = ["Neon"]
+    const filterByNeon = "Neon"
+    const filterByFindings = "Findings"
+    const filterByTonsOfRock = "Tons of Rock"
 
-    const getEventsByKeywords = async() => {
-        const keywords = filterByKeyword.join(",").toLocaleLowerCase() //fjerner at URL blir case sensitiv, det gjør det enklere å hente ut
-        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keywords}&apikey=AFEfcxa4XlCTGJA56Jk356h0NkfziiWD`) //Etter en del prøving og feiling her tenkte vi å prøve og gjøre keywords dynamisk
+    const getEventsByNeon = async() => {
+        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${filterByNeon}&apikey=AFEfcxa4XlCTGJA56Jk356h0NkfziiWD`) //Etter en del prøving og feiling her tenkte vi å prøve og gjøre keywords dynamisk
         .then(response => response.json())
         .then(data => {
             console.log(data)
             if(data._embedded && data._embedded.events) { //Sjekker om det som hentes ut har data lagret i disse nøklene. Om de ikke har det skal de ikke printes ut da de ikke eksisterer
-                setEventsByKeywords(data._embedded.events)
+                setEventsByNeon(data._embedded.events)
             } else {
-                console.log(`Ingen treff på søkeordet: ${keywords}`)
-            }
-            
+                console.log(`Ingen treff på søkeordet: ${filterByNeon}`) //Legger inn denne loggen for å vite om fethen har innhold, har den ikke får vi beskjed
+            }    
+      })
+      .catch(error => console.error("Feil ved henting av arrangementer:", error));
+    }
+
+    const getEventsByFindings = async() => {
+        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${filterByFindings}&apikey=AFEfcxa4XlCTGJA56Jk356h0NkfziiWD`) //Etter en del prøving og feiling her tenkte vi å prøve og gjøre keywords dynamisk
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            if(data._embedded && data._embedded.events) { 
+                setEventsByFindings(data._embedded.events)
+            } else {
+                console.log(`Ingen treff på søkeordet: ${filterByFindings}`) 
+            }    
+      })
+      .catch(error => console.error("Feil ved henting av arrangementer:", error));
+    }
+
+    const getEventsByTonsOfRock = async() => {
+        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${filterByTonsOfRock}&apikey=AFEfcxa4XlCTGJA56Jk356h0NkfziiWD`) //Etter en del prøving og feiling her tenkte vi å prøve og gjøre keywords dynamisk
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            if(data._embedded && data._embedded.events) { 
+                setEventsByFindings(data._embedded.events)
+            } else {
+                console.log(`Ingen treff på søkeordet: ${filterByTonsOfRock}`) 
+            }    
       })
       .catch(error => console.error("Feil ved henting av arrangementer:", error));
     }
 
     useEffect(() => {
-        getEventsByKeywords()
-    }, [eventsByKeywords]) //definerer her at hvis det skjer endringer i denne variabelen så skal det skje på nytt
+        getEventsByNeon()
+        getEventsByFindings()
+        getEventsByTonsOfRock()
+    }, []) 
 
     return(
         <>
        <h1>Utvalgte eventer</h1> 
-       <ul>
-        {eventsByKeywords.map(event => 
-            <li key={event.id}>{event?.name}</li>
-        )}
-       </ul>
+       <section className="neonevents">
+        <h2>{filterByNeon}</h2> 
+        <ul>
+            {eventsByNeon?.map(event => 
+                <li key={event.id}>{event?.name}</li>
+            )}
+        </ul>
+       </section>
+       <section>
+        <h2>{filterByFindings}</h2>
+        <ul>
+            {eventsByFindings?.map(event =>
+                <li key={event.id}>{event?.name}</li>
+            )}
+        </ul>
+       </section>
+       
         </>
     )
 }
