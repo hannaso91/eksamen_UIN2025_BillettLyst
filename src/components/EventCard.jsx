cd7c4278eb66ef831e6da6673891d8fa5c7e9427
hannaso91 <hannaso@hiof.no>
2025-05-02 12:51:30 +0200

    push endring

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -4,13 +4,14 @@ export default function EventCard ({festivals}){
     return(
         <>
         {festivals?.map(event => 
-            <Link key={event.id} to={`/event/${event.id}`}>
-                <article>
-                    <img src={event.images?.[0]?.url}  alt={event.name}/>
-                    <h3>{event?.name}</h3>
-                    <button>{`les mer om ${event.name}`}</button>
-                </article>
-            </Link>
+            <Link key={event.id} to={`/event/${event.id}`} className="event-card">
+            <article>
+              <img src={event.images?.[0]?.url} alt={event.name} />
+              <h3>{event.name}</h3>
+              <p>Les mer om {event.name}</p>
+            </article>
+          </Link>
+          
         )}
         </>
     )
