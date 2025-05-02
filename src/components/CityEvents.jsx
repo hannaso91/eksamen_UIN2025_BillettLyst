edfb83af9d7b6eae5fa8c149453b4a424ef24ebc
Signe-Sofie <142736190+Signe-Sofie@users.noreply.github.com>
2025-05-02 11:53:38 +0200

    vi prøver å få de ti eventene i hver by til å fungere

--- src/components/CityEvents.jsx
+++ src/components/CityEvents.jsx
@@ -2,13 +2,12 @@ import { useState } from "react"
 
 export default function CityEvents() {
 
-    const {}
-
     const [selectedCity, setSelectedCity] = useState()
-    const handleClick = async() => {
-        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?city=${selectedCity}size=10&apikey=AFEfcxa4XlCTGJA56Jk356h0NkfziiWD`)
+    const handleClick = async(city) => {
+        setSelectedCity(city)
+        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?city=${City}size=10&apikey=AFEfcxa4XlCTGJA56Jk356h0NkfziiWD`)
         .then(response => response.json())
-        .then((data) => console.log(data))
+        .then((data) => console.log("city",data))
         .catch(error => console.error("neinei, det går ikke", error))
     }
 
