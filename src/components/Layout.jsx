07c91e0d584a54ffc5bf005cb630e1a68348fdf0
hannaso91 <hannaso@hiof.no>
2025-05-04 21:13:13 +0200

    pusher logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import { Link, NavLink } from "react-router-dom";
 import Nav from "./Nav";
+import {Logo} from "../pictures/BillettLyst.png"
 
 export default function Layout({children}) {
     return(
         <>
         <header>
             <div className="logo">
-                <Link to="/"><img src="#" alt="Vår logo"/></Link>
+                <Link to="/"><img src={Logo} alt="Vår logo"/></Link>
             </div>
            <Nav /> 
            <NavLink to="/dashboard">Logg inn</NavLink>
