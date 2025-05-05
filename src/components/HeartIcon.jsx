b9de16b7d2107e60be18499a387ee8e38438ca65
hannaso91 <hannaso@hiof.no>
2025-05-05 11:29:54 +0200

    pusher

--- src/components/HeartIcon.jsx
+++ src/components/HeartIcon.jsx
@@ -1,14 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart as faHeartSolid } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+
 import { useState } from 'react';
 
 export default function HeartIcon({id, storageLiked}) {
     const [liked, setLiked] = useState (false) //Setter til default false så hjertet ikke er klikket 
     const handleClick = (e) => {
-        e.preventDefault()
-        const wishList = JSON.parse(storageLiked)
-        setLiked(!liked) //Denne bytter mellom true og false, utropstegnet bytter fra den staten den allerede er i, setter det motsatte
+        e.preventDefault();
+        const wishList = JSON.parse(storageLiked || '[]');
+        let updatedList;
+    
+        if (liked) {
+            updatedList = wishList.filter(item => item !== id); 
+        } else {
+            updatedList = [...wishList, id]; 
+        }
+    
+        localStorage.setItem("likedItems", JSON.stringify(updatedList));
+        setLiked(!liked);
     }
+    
     return(
         <button onClick={handleClick }>
             <FontAwesomeIcon icon={faHeartSolid} color={liked ? "red" : "gray"} />
