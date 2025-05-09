2726c3561bc0ed8e331f7eeee9adbcecd082c572
Signe-Sofie <142736190+Signe-Sofie@users.noreply.github.com>
2025-05-09 10:28:48 +0200

    some funker snart

--- src/components/Some.jsx
+++ src/components/Some.jsx
@@ -4,14 +4,18 @@ import { faFacebook, faInstagram, faSpotify } from '@fortawesome/free-brands-svg
 export default function Some({event}) {
 
     const facebook = event.externalLinks.facebook[0].url
-    const instagram = event.externalLinks.instagram
-    const spotify = event.externalLinks.spotify
+    const instagram = event.externalLinks.instagram[0].url
+    const spotify = event.externalLinks.spotify[0].url
 
     const facebookExists = facebook ? <a href={facebook}  target="_blank"><FontAwesomeIcon icon={faFacebook}/></a> : null
+    const instagramExists = instagram ? <a href={instagram} target="_blank"><FontAwesomeIcon icon={faInstagram} /></a> : null
+    const spotifyExists = spotify ? <a href={spotify} target="_blank"><FontAwesomeIcon icon={faSpotify} /></a> : null
     
     return(
         <ul>
             <li>{facebookExists}</li>
+            <li>{instagramExists}</li>
+            <li>{spotifyExists}</li>
         </ul>
         
     )
