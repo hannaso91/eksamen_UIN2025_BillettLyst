1581c44290ec657b51890729e0bed1ebdd7db425
Signe-Sofie <142736190+Signe-Sofie@users.noreply.github.com>
2025-05-09 10:04:48 +0200

    får ikke opp iconer, vi prøver i fellesskap

--- src/components/Some.jsx
+++ src/components/Some.jsx
@@ -1,5 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default function Some({event}) {
 
+    const facebook = event.externalLinks.facebook
+    const instagram = event.externalLinks.instagram
+    const spotify = event.externalLinks.spotify
     
-
+    return(
+        {
+            if (facebook)
+                {
+                    <ul><li>
+                        <a href={facebook}><FontAwesomeIcon icon={faFacebook} /></a>
+                </li></ul>
+            }   
+        }
+        
+    )
 }
