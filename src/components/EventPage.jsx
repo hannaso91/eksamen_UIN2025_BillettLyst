a0d75c1f17f56b1da027eb177301b5025bc78e7e
hannaso91 <hannaso@hiof.no>
2025-05-09 09:52:52 +0200

    pusher det som er gjort

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import ArtistCard from "./ArtistCard"
 import EventCard from "./EventCard"
+import Some from "./Some"
 import "../styles/eventPage.scss"
 
 
@@ -15,7 +16,7 @@ export default function EventPage() {
     const getEvent = async () => {
         fetch(`https://app.ticketmaster.com/discovery/v2/attractions/${id}.json?apikey=AFEfcxa4XlCTGJA56Jk356h0NkfziiWD`)
         .then(response => response.json())
-        .then((data) => 
+        .then((data) =>
             setEvent(data))
         .catch((error) => console.error("Steike da! Det skjedde noe galt, er du sjokkert? NEI! Ikke jeg heller", error))
     }
@@ -51,6 +52,7 @@ export default function EventPage() {
                 <li>{event.classifications?.[0]?.genre?.name}</li>
             </ul>
             <h3 className="soMe">Følg oss på sosiale medier!</h3>
+            <Some />
             <article> {/*Legges inn i Some.jsx når det fungerer med riktig href osv*/}
             </article>
             <hr></hr>
