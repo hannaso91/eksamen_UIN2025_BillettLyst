c56c33b0fbdc6c717d508013d2303d65ce3d483d
hannaso91 <hannaso@hiof.no>
2025-05-10 17:57:20 +0200

    artistcard ferdig kommentert

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -2,30 +2,26 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import EventCard from "./EventCard"
 import HeartIcon from "./HeartIcon"
-import VenuesCategoryPage from "./VenuesCategoryPage"
 import AttractionsCategoryPage from "./AttractionsCategoryPage"
 import SearchForm from "./SearchForm"
 
 export default function CategoryPage ({storageLiked}) {
 
     const {slug} = useParams()
-    const [date, setDate] = useState(() => {
-        const today = new Date();
-        return today.toISOString()
-    })
-    const [country, setCountry] = useState("Norge") // Setter inn default slik at noe alltid er der når siden lastes
+    const [date, setDate] = useState()
+    const [country, setCountry] = useState("Norge") // Setter inn default slik at noe alltid er der når siden lastes, uten default verdi var siden tom ved rendering
     const [categoryCity, setCity] = useState("Oslo")
     const [eventsAPI, setEventsAPI] = useState(() => [])
     const [attractions, setAttractions] = useState([])
     const [venues, setVenues] = useState([])
     const [keyword, setKeyword] = useState("")
    
-
+    // Ettersom det fikke fungerte i starten, måtte vi begynne å feilsøke. Det første var å finne ut om slug var riktig
     console.log("slug", slug)
     console.log(date)
    
 
-    //Vi skjønte tidlig at APIet ikke vil forstå norsk siden URL er på norsk ville ikke det gitt resultater i fetchen. Vi valgte derfor å lage en egen variabel som gjør det om til engelsk og bruke slug for å hente ut riktig
+    //Vi skjønte tidlig at APIet ikke vil forstå norsk siden slug fra URL er på norsk ville ikke det gitt resultater i fetchen. Vi valgte derfor å lage en egen variabel som gjør det om til engelsk og bruke slug for å hente ut riktig
     // Dette må matche det som er i APIet for å få hentet ut noe
     const categoryEnglish = {
         musikk: "music",
@@ -34,10 +30,10 @@ export default function CategoryPage ({storageLiked}) {
     }
 
     //Lager så en variabel som henter inn basert på riktig slug
-    const apiCategory = categoryEnglish[slug] || "music"
+    const apiCategory = categoryEnglish[slug] 
     console.log("apiCategory", apiCategory)
-    //Det samme må gjøres med land, inputen er på norsk og den må gjøres om til countrycode siden det er det som blir brukt i APIet1
 
+    //Det samme må gjøres med land, inputen er på norsk og den må gjøres om til countrycode siden det er det som blir brukt i APIet. Sjekket i explorer for hvert land
     const countryCode = {
         Norge: "NO",
         Sverige: "SE",
@@ -46,13 +42,13 @@ export default function CategoryPage ({storageLiked}) {
         Tyskland: "DE"
     }
 
-    const code = countryCode[country]
+    const code = countryCode[country] // Det som ligger i staten blir gjort til en countrycode som vi kan bruke i fetch url
 
-    const keywordcheck = keyword ? keyword : `${categoryCity} ${apiCategory}`; //hvis keyword eksisterer så skal det søkes på den uavhengig av noe annet
+    const keywordcheck = keyword ? keyword : `${categoryCity} ${apiCategory}`; //hvis keyword eksisterer så skal det søkes på den uavhengig av noe annet, dette gjør at bruker kan søke på hva som helst og få opp resultatet knyttet til søket
+    //Denne måten å gjøre det på fungerte akkurat som ønsket og ble testet for å se om fungerte. Litt prøve og feile fremgangsmåte
 
 
-    const getEventsInEventsAPI = async () => {
-        
+    const getEventsInEventsAPI = async () => { 
         fetch(`https://app.ticketmaster.com/discovery/v2/suggest?apikey=8jut1LP6C3Z6ZSSJU3PCMoler5qA4oZW&keyword=${keywordcheck}&locale=*&countryCode=${code}`)
             .then(response => response.json())
             .then(data => {
@@ -62,13 +58,13 @@ export default function CategoryPage ({storageLiked}) {
                 setVenues(data._embedded?.venues || []);
             })
             .catch(error => {
-                console.error("Steike da! Det skjedde noe galt, er du sjokkert? NEI! Ikke jeg heller", error);
+                console.error("Noe gikk galt i fetch i filtreringen", error);
             });
     }
 
     useEffect(() => {
         getEventsInEventsAPI()
-    }, [slug, categoryCity, code, keyword, date])
+    }, [slug, categoryCity, country, keyword, date]) //har satt inn noen avhengigheter for at useeffect kjører. Den skal også kjøres hver gang det er endringer i noen av disse statene/variablene/params
 
 
     return(
@@ -127,11 +123,11 @@ export default function CategoryPage ({storageLiked}) {
         </section>
         <section>
             <h3>Spillesteder/eventsteder</h3>
-            {venues.map(venue => 
-                <div key={venue.id}>
-                    <VenuesCategoryPage venue={venue} storageLiked={storageLiked} />
+            {venues.map(attraction => //Bruker attraction og sender inn her siden det er det som blir tatt imot i komponentet, ettersom det komponentet blir gjenbrukt må også samme prop sendes
+                <div key={attraction.id}>
+                    <AttractionsCategoryPage attraction={attraction} storageLiked={storageLiked}/>
                     <span>
-                        <HeartIcon id={venue.id} />
+                        <HeartIcon id={attraction.id} />
                     </span>
                 </div>
             )}
